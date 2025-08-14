008f5015c2f1859eb94249b9034eabd86ce93d0c
Rissav25Pokhrel <risav.pok@gmail.com>
2025-08-14 11:27:42 +0545

    Refactor: Simplify CoordinateConverter to an abstract class and add extensions for coordinate conversion

--- lib/src/map_core/coordinate_converter.dart
+++ lib/src/map_core/coordinate_converter.dart
@@ -1,27 +1,8 @@
-import 'dart:math';
 import 'dart:ui';
 import 'package:baato_api/baato_api.dart';
-import 'package:maplibre_gl/maplibre_gl.dart';
 
-/// A utility class that handles coordinate conversions between screen positions and geographic coordinates.
-///
-/// This class provides methods to convert between screen coordinates (as [Offset]s) and
-/// geographic coordinates (as [BaatoCoordinate]s) using the MapLibre map controller.
-///
-/// The coordinate converter is an essential component for interactive map features
-/// such as placing markers at tap locations, determining what geographic features
-/// are under a user's finger, and implementing custom UI overlays that need to
-/// align with map features.
-class CoordinateConverter {
-  /// The underlying MapLibre map controller used for coordinate conversions
-  final MapLibreMapController _mapLibreMapController;
-
-  /// Creates a new CoordinateConverter with the specified MapLibre controller
-  ///
-  /// [_mapLibreMapController] is the MapLibre controller that will be used
-  /// for all coordinate conversion operations
-  CoordinateConverter(this._mapLibreMapController);
-
+/// Abstract interface for converting between screen positions and geographic coordinates.
+abstract class CoordinateConverter {
   /// Converts a screen location to geographic coordinates.
   ///
   /// This method translates a point on the screen (such as a tap location) to its
@@ -42,15 +23,7 @@ class CoordinateConverter {
   ///   }
   /// }
   /// ```
-  Future<BaatoCoordinate?> toLatLng(Offset screenLocation) async {
-    final point = await _mapLibreMapController.toLatLng(
-      Point(screenLocation.dx, screenLocation.dy),
-    );
-    return BaatoCoordinate(
-      latitude: point.latitude,
-      longitude: point.longitude,
-    );
-  }
+  Future<BaatoCoordinate?> toLatLng(Offset screenLocation);
 
   /// Converts geographic coordinates to a screen location.
   ///
@@ -73,9 +46,5 @@ class CoordinateConverter {
   ///   }
   /// }
   /// ```
-  Future<Offset?> toScreenLocation(BaatoCoordinate coordinate) async {
-    final latLng = LatLng(coordinate.latitude, coordinate.longitude);
-    final point = await _mapLibreMapController.toScreenLocation(latLng);
-    return Offset(point.x.toDouble(), point.y.toDouble());
-  }
+  Future<Offset?> toScreenLocation(BaatoCoordinate coordinate);
 }
